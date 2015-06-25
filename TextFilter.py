1933472137b42070895b6ed010298c7fb9703a26
tpsatish95 <tpsatish95@gmail.com>
2015-06-25 16:41:06 +0530

    Licence Header Added

--- TextFilter.py
+++ TextFilter.py
@@ -1,5 +1,29 @@
+'''
+   Copyright 2015 Satish Palaniappan
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   '''
+# Author : Satish Palaniappan
+__author__ = "Satish Palaniappan"
+
 import pickle
 import re, collections
+
+### Insert Current Path
+import os, sys, inspect
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
 
 from Twokenize import twokenize
 from Twokenize import emoticons
@@ -13,13 +37,6 @@ stemmer=PorterStemmer()
 
 # import spell check module
 from SpellCheck.Corrector import spellCheck
-
-### Insert Current Path
-import os, sys, inspect
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
-if cmd_folder not in sys.path:
-	sys.path.insert(0, cmd_folder)
-
 
 path = cmd_folder+"/models/"
 
@@ -64,7 +81,7 @@ class Filter(object):
 				try:
 					#### Check Dict and set flag
 					if self.wordDict[word] == 1:
-						print(word)
+						# print(word)
 						word =	word
 				except:
 					flagNonDict = 1
@@ -72,6 +89,7 @@ class Filter(object):
 						score = self.emoticons[word]
 						emo = emoticons.analyze_tweetHeavy(word)
 						word = emo + "#("+ str(score) +")#"
+						# print(word)
 					except:
 						try:
 							#Normalize Acronyms
